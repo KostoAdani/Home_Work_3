cbcbc7359a34453c0027758f858fc6617835ed9f
KostoAdani <www.footballmen@mail.ru>
2025-09-07 22:09:55 +0300

    Update MyArrayList.java

--- src/HomeWork_3/MyArrayList.java
+++ src/HomeWork_3/MyArrayList.java
@@ -5,25 +5,39 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
-public class MyArrayList <T>{
-    private final ArrayList<T> elements;
-
-    public MyArrayList(){
-        elements = new ArrayList<>();
+public MyArrayList() {
+        elements = new Object[10];
+        size = 0;
     }
-    public void add(T element){
-        elements.add(element);
+     public void add(Object value) {
+        if (size == elements.length) {
+            resize();
+        }
+        elements[size++] = value;
     }
-    public T get(int index){
-        return elements.get(index);
+     public Object get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return elements[index];
     }
-    public void remove(int index){
-        elements.remove(index);
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        elements[--size] = null; // Удаляем ссылку на последний элемент
     }
-    public void addAll(List<T>other){
-        elements.addAll(other);
+    public void addAll(MyArrayList other) {
+        for (int i = 0; i < other.size; i++) {
+            add(other.get(i));
+        }
     }
-    public List<T> getElements(){
-        return elements;
+  private void resize() {
+        Object[] newArray = new Object[elements.length * 2];
+        System.arraycopy(elements, 0, newArray, 0, elements.length);
+        elements = newArray;
     }
 }
