fa70643b334ecf4f131bd2a74b30e784d9de791d
KostoAdani <www.footballmen@mail.ru>
2025-09-07 22:05:08 +0300

    Update MyHashSet.java

--- src/HomeWork_3/MyHashSet.java
+++ src/HomeWork_3/MyHashSet.java
@@ -3,25 +3,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
 public class MyHashSet<T> {
     private final List<T> elements;
 
 
-    public MyHashSet(){
+    public MyHashSet() {
         elements = new ArrayList<>();
     }
+
     public void insert(T element) {
         if (!elements.contains(element)) {
             elements.add(element);
-
         }
     }
-    public boolean contains(T element){
-        return elements.contains(element);
+public boolean contains(T element) {
+        if (element == null) {
+            for (T e : elements) {
+                if (e == null) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            for (T e : elements) {
+                if (element.equals(e)) {
+                    return true;
+                         }
+            }
+            return false;
+        }
     }
-    public boolean remove(T element){
+
+    public boolean remove(T element) {
         return elements.remove(element);
     }
 }
-
